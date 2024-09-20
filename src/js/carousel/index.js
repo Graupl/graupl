e8b855ca58faa89a91d7ee167c9497e34f89ac90
Nick Milton <nickdjm@gmail.com>
2024-09-20 15:18:39 -0400

    refactor: use 3 classes for transitioning slides

--- src/js/carousel/index.js
+++ src/js/carousel/index.js
@@ -4,26 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const carousels = [];
 
   document.querySelectorAll(".carousel").forEach((carouselElement) => {
-    const carouselItems = carouselElement.querySelectorAll(".carousel-item");
-    const autoplayButton = carouselElement.querySelector(".autoplay");
-    const nextButton = carouselElement.querySelector(".next");
-    const previousButton = carouselElement.querySelector(".previous");
-
     const carousel = new Carousel({
       carouselElement,
-      carouselItems,
-      autoplayButton,
-      nextButton,
-      previousButton,
       initialize: true,
     });
 
     carousels.push(carousel);
   });
 
-  const gruapl = window.Graupl || {};
+  const graupl = window.Graupl || {};
 
-  gruapl.carousels = carousels;
+  graupl.carousels = carousels;
 
-  window.Graupl = gruapl;
+  window.Graupl = graupl;
 });
