f9421f1bef28020e5222c9c9904c39a579fa083b
sstapleton <sstapleton@coldfrontlabs.ca>
2024-09-27 15:24:26 -0400

    feat: allow build of different file types

---
+++ vite.config.js
@@ -0,0 +1,57 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import process from "process";
+
+// Get the current directory path.
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+// The type of menu to build from the BUILD_TYPE environment variable.
+// If BUILD_TYPE is not set, the default build is used
+const buildType = process.env.BUILD_TYPE ?? "default";
+
+// Supported build types.
+const formats = ["iife", "es", "cjs"];
+
+// The library options for the different menus.
+const lib = {
+  default: {
+    entry: resolve(__dirname, "build.js"),
+    name: "Graupl",
+    formats,
+    fileName: (format) => `graupl.${format}.js`,
+  },
+  Alert: {
+    entry: resolve(__dirname, "/src/js/alert/Alert.js"),
+    name: "Alert",
+    formats,
+    fileName: (format) => `component/alert.${format}.js`,
+  },
+  Carousel: {
+    entry: resolve(__dirname, "/src/js/carousel/Carousel.js"),
+    name: "Carousel",
+    formats,
+    fileName: (format) => `component/carousel.${format}.js`,
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'dist/js',
+    lib: lib[buildType] || lib.default,
+    emptyOutDir: false,
+  },
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      host: "localhost",
+    },
+  },
+  test: {
+    environment: "jsdom",
+    environmentOptions: {
+      pretendToBeVisual: true,
+    },
+  },
+});
