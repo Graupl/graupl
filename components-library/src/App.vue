641d02a356a30679cdaca7e1c674f4be62632d43
Nick Milton <git@ndjm.ca>
2025-09-10 18:07:44 +0000

    feat: begin implementing side bar

--- components-library/src/App.vue
+++ components-library/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import clTheme from "./views/Theme.vue";
 import clComponents from "./views/Components.vue";
 import clLayout from "./views/Layout.vue";
 import logoUrl from "../../logo.svg";
+import NavigationShelfGenerator from "../../packages/navigation-shelf/src/js/navigation-shelf/generator.js";
+
+onMounted(() => {
+  NavigationShelfGenerator();
+});
 
 const themeToggle = ref(false);
 const preferredTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -15,7 +20,92 @@ document.documentElement.classList.add(`${setTheme}-mode`);
 </script>
 
 <template>
-  <main id="cl-app" class="container">
+  <header>
+    <button class="navigation-shelf-toggle">Toggle shelf</button>
+    <div class="navigation-shelf bg-primary-700 text-primary-100">
+      <div class="navigation-shelf-header"></div>
+      <div class="navigation-shelf-content w-shelf-open">
+        <nav
+          class="navigation display-flex flex-col"
+          data-graupl-menu-type="DisclosureMenu"
+          data-graupl-menu-options="{'optionalKeySupport': true}"
+        >
+          <ul class="menu flex-col">
+            <li class="menu-item flex-wrap flex-row">
+              <a class="menu-link flex-grow w-min-content" href="#view-theme"
+                >Theme</a
+              >
+            </li>
+            <li class="menu-item submenu-item">
+              <button class="menu-link button link submenu-toggle">
+                Components
+              </button>
+              <ul class="submenu min-w-full position-relative">
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-links">Links</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-quotation">Quotation</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-buttons">Buttons</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-lists">Lists</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-details-disclosure"
+                    >Details</a
+                  >
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-tables">Tables</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-form-elements">Forms</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-navigation">Navigation</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-cards">Cards</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-alerts">Alerts</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-accordions">Accordions</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="#section-carousel">Carousel</a>
+                </li>
+              </ul>
+            </li>
+            <li class="menu-item">
+              <a class="menu-link" href="#view-layout">Layout</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div
+        class="navigation-shelf-footer w-shelf-current display-flex flex-wrap"
+      >
+        <button
+          class="button navigation-shelf-lock-toggle w-full"
+          aria-label="Toggle shelf lock"
+        ></button>
+        <button
+          class="button navigation-shelf-hover-toggle w-full"
+          aria-label="Toggle shelf hover"
+        ></button>
+        <button
+          class="button navigation-shelf-side-toggle w-full"
+          aria-label="Toggle side"
+        ></button>
+      </div>
+    </div>
+  </header>
+  <main id="cl-app" class="container shelf-aware">
     <div class="flex-columns">
       <div class="col-8">
         <h1
@@ -49,5 +139,24 @@ document.documentElement.classList.add(`${setTheme}-mode`);
 
 <style lang="scss">
 @use "../../scss/graupl";
+@use "../../packages/icons/scss/icon";
+@use "../../packages/navigation-shelf/scss/navigation-shelf";
+
+// :root {
+//   --graupl-navigation-shelf-closed-display: none;
+//   --graupl-navigation-shelf-closed-opacity: 0;
+//   --graupl-navigation-shelf-starting-opacity: 0;
+//   --graupl-navigation-shelf-transitioning-opacity: 0;
+//   --graupl-navigation-shelf-min-width: 36ch;
+// }
+
+// .left-side {
+//   --graupl-navigation-shelf-closed-transform: translateX(-100%);
+//   --graupl-navigation-shelf-transitioning-transform: translateX(-100%);
+// }
+
+// .right-side {
+//   --graupl-navigation-shelf-closed-transform: translateX(100%);
+//   --graupl-navigation-shelf-transitioning-transform: translateX(100%);
+// }
 </style>
-"
