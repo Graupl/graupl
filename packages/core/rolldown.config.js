7f5560f564b7c5bc91eb690f4a13c90e1409d916
Nick Milton <git@ndjm.ca>
2025-10-23 18:21:44 +0000

    chore: reorder rolldown order

--- packages/core/rolldown.config.js
+++ packages/core/rolldown.config.js
@@ -131,17 +131,6 @@ export default defineConfig([
     plugins,
   },
   {
-    input: "./src/js/navigation/generator.js",
-    output: ["iife", "es", "cjs"].map((format) => ({
-      file: `dist/js/generator/navigation.${format}.js`,
-      format,
-      name: "NavigationGenerator",
-      sourcemap: true,
-    })),
-    platform: "browser",
-    plugins,
-  },
-  {
     input: "./src/js/disclosure/Disclosure.js",
     output: ["iife", "es", "cjs"].map((format) => ({
       file: `dist/js/component/disclosure.${format}.js`,
@@ -158,6 +147,17 @@ export default defineConfig([
       file: `dist/js/generator/disclosure.${format}.js`,
       format,
       name: "DisclosureGenerator",
+      sourcemap: true,
+    })),
+    platform: "browser",
+    plugins,
+  },
+  {
+    input: "./src/js/navigation/generator.js",
+    output: ["iife", "es", "cjs"].map((format) => ({
+      file: `dist/js/generator/navigation.${format}.js`,
+      format,
+      name: "NavigationGenerator",
       sourcemap: true,
     })),
     platform: "browser",
