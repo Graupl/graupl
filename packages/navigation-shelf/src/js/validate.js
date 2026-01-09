d6f0943b9db527da1bca6dd2228dc5931da0dd94
Nick Milton <git@ndjm.ca>
2026-01-09 17:17:43 +0000

    feat(navigation shelf): update js to use new component base class

--- packages/navigation-shelf/src/js/validate.js
+++ packages/navigation-shelf/src/js/validate.js
@@ -8,10 +8,17 @@
  *
  * Will return `{ status: true }` if the check is successful.
  *
- * @param  {Object<string>}          values - The value(s) to check.
- * @return {Object<boolean, string>}        - The result of the check.
+ * @param  {Object<string>}          values                       - The value(s) to check.
+ * @param  {object}                  [options = {}]               - The options for the check.
+ * @param  {boolean}                 [options.shouldThrow = true] - Whether to throw on error or return it.
+ * @return {Object<boolean, string>}                              - The result of the check.
  */
-export function isValidSideType(values) {
+export function isValidSideType(values, { shouldThrow = true } = {}) {
+  const result = {
+    status: true,
+    errors: [],
+  };
+
   try {
     if (typeof values !== "object") {
       const type = typeof values;
@@ -24,12 +31,17 @@ export function isValidSideType(values) {
     const validTypes = ["left", "right"];
 
     for (const key in values) {
-      if (!validTypes.includes(values[key])) {
-        throw new TypeError(
-          `${key} must be one of the following values: ${validTypes.join(
-            ", "
-          )}. "${values[key]}" given.`
-        );
+      try {
+        if (!validTypes.includes(values[key])) {
+          throw new TypeError(
+            `${key} must be one of the following values: ${validTypes.join(
+              ", "
+            )}. "${values[key]}" given.`
+          );
+        }
+      } catch (error) {
+        result.status = false;
+        result.errors.push(error);
       }
     }
 
@@ -38,9 +50,13 @@ export function isValidSideType(values) {
       error: null,
     };
   } catch (error) {
-    return {
-      status: false,
-      error,
-    };
+    result.status = false;
+    result.errors.push(error);
   }
+
+  if (shouldThrow && !result.status) {
+    throw result.errors[0];
+  }
+
+  return result;
 }
