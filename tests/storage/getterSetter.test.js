0fc2b06aa6431e0cef63ea74aeda3f664838883c
Nick Milton <git@ndjm.ca>
2026-02-11 21:02:19 +0000

    test: fix paths

--- tests/storage/getterSetter.test.js
+++ tests/storage/getterSetter.test.js
@@ -3,7 +3,7 @@
  */
 
 import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
-import StorageManager from "../../packages/core/src/js/StorageManager.js";
+import StorageManager from "../../packages/core/src/js/storage/StorageManager.js";
 import * as validation from "../../packages/core/src/js/validate.js";
 
 const scope = "TestStorage";
