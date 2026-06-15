3d7ce8ddf0a77efab3437f141127a30987f3b4c1
Nick Milton <git@ndjm.ca>
2026-06-15 16:37:06 -0400

    test(vrt): add link

---
+++ tests/components/link/link.js
@@ -0,0 +1,28 @@
+import { h } from "vue";
+import "../../../dist/css/graupl.css";
+
+export default {
+  props: {
+    variant: {
+      type: String,
+      default: "",
+    },
+    state: {
+      type: String,
+      default: "",
+    },
+    attributes: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const attributes = props.attributes || {};
+    attributes.class = attributes.class || [];
+
+    attributes.class.push(props.variant);
+    attributes.class.push(props.state);
+
+    return () => h("a", attributes, "Click me");
+  },
+};
