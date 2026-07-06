f4ceb680459095bc82b684d29b68805a25699502
Jessica <jdash@coldfrontlabs.ca>
2026-07-06 14:43:55 +0000

    build: menu test test

--- tests/components/menu/menu.browser.test.js
+++ tests/components/menu/menu.browser.test.js
@@ -14,7 +14,7 @@ describe("Menu Component", () => {
 
       await document.fonts.ready;
 
-      await expect(screen.getByTestId("menu-item-2")).toMatchScreenshot();
+      await expect(screen.getByTestId("menu")).toMatchScreenshot();
     });
   });
 });
