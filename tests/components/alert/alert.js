16a82c8c69a7bc61f6d3bd5a96347f67ddf3d675
Jessica <jdash@coldfrontlabs.ca>
2026-07-06 15:11:44 +0000

    build: alert test test

---
+++ tests/components/alert/alert.js
@@ -0,0 +1,26 @@
+import { h } from "vue";
+import BasicComponent from "../component/basic-component.js";
+
+export default {
+  props: {
+    variant: {
+      type: String,
+      default: "",
+    },
+    text: {
+      type: String,
+      default: "Alert",
+    },
+    attributes: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    props.attributes.class = props.attributes.class || [];
+
+    props.attributes.class.push("alert");
+
+    return () => h(BasicComponent, { ...props, tag: "span" });
+  },
+};
