8285c31cf56fd7f7a17ac334ac528e17611b8ff4
Jessica <jdash@coldfrontlabs.ca>
2026-07-07 22:15:59 +0000

    build: card test test

--- tests/components/card/card.js
+++ tests/components/card/card.js
@@ -9,25 +9,28 @@ export default {
     },
     text: {
       type: Object,
-      default:
-        h(BasicComponent, {
-          tag: "div",
-          attributes: { class: "card-content" },
-          text: h(BasicComponent, {
+      default: () => {
+        (() =>
+          h(BasicComponent, {
             tag: "div",
-            attributes: { class: "card-body" },
-            text: h(BasicComponent, { tag: "p", text: "Card" }),
+            attributes: { class: "card-content" },
+            text: h(BasicComponent, {
+              tag: "div",
+              attributes: { class: "card-body" },
+              text: h(BasicComponent, { tag: "p", text: "Card" }),
+            }),
           }),
-        }) +
-        h(BasicComponent, {
-          tag: "div",
-          attributes: { class: "card-footer" },
-          text: h(BasicComponent, {
-            tag: "a",
-            attributes: { class: "button-stretched", href: "/" },
-            text: "The Action",
-          }),
-        }),
+          () =>
+            h(BasicComponent, {
+              tag: "div",
+              attributes: { class: "card-footer" },
+              text: h(BasicComponent, {
+                tag: "a",
+                attributes: { class: "button-stretched", href: "/" },
+                text: "The Action",
+              }),
+            }));
+      },
     },
     attributes: {
       type: Object,
