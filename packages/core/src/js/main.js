529a9fdb05b9490600847777913c7eec5e95f130
Nick Milton <git@ndjm.ca>
2026-07-22 13:23:00 +0000

    fix: add missing components to full JS build

--- packages/core/src/js/main.js
+++ packages/core/src/js/main.js
@@ -1,13 +1,19 @@
 import accordionGenerator from "./accordion/generator.js";
 import alertGenerator from "./alert/generator.js";
+import breadcrumbGenerator from "./breadcrumb/generator.js";
 import carouselGenerator from "./carousel/generator.js";
-import navigationGenerator from "./navigation/generator.js";
 import disclosureGenerator from "./disclosure/generator.js";
+import navigationGenerator from "./navigation/generator.js";
+import tabsGenerator from "./tabs/generator.js";
+import tooltipGenerator from "./tooltip/generator.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   accordionGenerator();
   alertGenerator();
+  breadcrumbGenerator();
   carouselGenerator();
-  navigationGenerator();
   disclosureGenerator();
+  navigationGenerator();
+  tabsGenerator();
+  tooltipGenerator();
 });
