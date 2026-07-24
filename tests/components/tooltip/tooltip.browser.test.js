93738fa44a212e0db1f2467d0ddf3e490747c57c
Jessica <jdash@coldfrontlabs.ca>
2026-07-24 20:16:42 +0000

    feat: tooltip and tabs tests

--- tests/components/tooltip/tooltip.browser.test.js
+++ tests/components/tooltip/tooltip.browser.test.js
@@ -15,7 +15,7 @@ describe("Disclosure Component", () => {
       await document.fonts.ready;
 
       const toggleButton = screen.getByRole("button").first();
-      await toggleButton.click();
+      toggleButton.click();
 
       await expect(toggleButton).toHaveClass("tooltip-toggle");
 
