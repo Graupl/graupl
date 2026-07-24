69f7619280690acd810baac0e9d6d5d8bcf84103
Jessica <jdash@coldfrontlabs.ca>
2026-07-24 16:54:18 +0000

    feat: disclosure test

--- tests/components/disclosure/disclosure.js
+++ tests/components/disclosure/disclosure.js
@@ -20,16 +20,18 @@ export default {
     props.attributes["data-testid"].push("disclosure");
 
     return () =>
-      h(
-        "div",
-        { "data-testid": "disclosure" },
+      h("div", { "data-testid": "disclosure" }, [
+        h(BasicComponent, {
+          class: "disclosure-toggle " + props.variant,
+          tag: "button",
+        }),
         h(BasicComponent, {
           tag: "div",
           ...props,
           text: h("div", { class: "disclosure-content" }, [
             h("p", "Disclosure"),
           ]),
-        })
-      );
+        }),
+      ]);
   },
 };
