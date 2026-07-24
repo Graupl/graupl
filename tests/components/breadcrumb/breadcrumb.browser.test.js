806a2be815d524da43a977bc7190055c8607b417
Jessica <jdash@coldfrontlabs.ca>
2026-07-24 15:38:02 +0000

    feat: breadcrumb test attempt

--- tests/components/breadcrumb/breadcrumb.browser.test.js
+++ tests/components/breadcrumb/breadcrumb.browser.test.js
@@ -24,7 +24,7 @@ describe("Breadcrumb Component", () => {
       await document.fonts.ready;
 
       await expect(screen.getByText("Home")).toBeVisible();
-      await expect(screen.class("class", "breadcrumb")).toMatchScreenshot();
+      await expect(screen.class("breadcrumb")).toMatchScreenshot();
     });
   });
 });
