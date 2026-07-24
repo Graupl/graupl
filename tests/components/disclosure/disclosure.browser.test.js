b278673cab565dd3d135dd6746c68c97cc5b245d
Jessica <jdash@coldfrontlabs.ca>
2026-07-24 13:34:26 +0000

    feat: disclosure test attempt

--- tests/components/disclosure/disclosure.browser.test.js
+++ tests/components/disclosure/disclosure.browser.test.js
@@ -7,21 +7,14 @@ import { variants } from "../defaults.js";
 describe("Disclosure Component", () => {
   describe.for(variants)("%s disclosure", async (variant) => {
     it("Should match screenshot", async () => {
-      const screen = render(
-        (h("button", {
-          class: "disclosure-toggle " + variant,
-          type: "button",
-        }),
-        Component,
-        {
-          props: {
-            variant,
-            text: h("div", { class: "disclosure-content" }, [
-              h("p", "Disclosure"),
-            ]),
-          },
-        })
-      );
+      const screen = render(Component, {
+        props: {
+          variant,
+          text: h("div", { class: "disclosure-content" }, [
+            h("p", "Disclosure"),
+          ]),
+        },
+      });
 
       await document.fonts.ready;
 
--- tests/components/disclosure/disclosure.js
+++ tests/components/disclosure/disclosure.js
@@ -17,6 +17,9 @@ export default {
     props.attributes.class.push("disclosure");
 
     return () => [
+      h("button", {
+        class: "disclosure-toggle " + props.variant,
+      }),
       h(BasicComponent, {
         tag: "div",
         ...props,
