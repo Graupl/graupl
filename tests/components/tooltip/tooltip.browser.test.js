9dea8a47c8a1f477ab712d67bf1e9074b8462764
Jessica <jdash@coldfrontlabs.ca>
2026-07-27 15:20:52 +0000

    feat: tooltips adjust test

--- tests/components/tooltip/tooltip.browser.test.js
+++ tests/components/tooltip/tooltip.browser.test.js
@@ -14,7 +14,7 @@ describe("Tooltip Component", () => {
 
       await document.fonts.ready;
 
-      const toggleButton = screen.getByRole("button");
+      const toggleButton = screen.getByRole("button", { name: "" }).first();
       await toggleButton.click();
 
       await expect(screen.getByTestId("tooltip")).toMatchScreenshot();
