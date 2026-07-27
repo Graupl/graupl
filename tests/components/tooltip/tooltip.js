bd91e3c756a342d0da2cac4d0ef28aaba8f19914
Jessica <jdash@coldfrontlabs.ca>
2026-07-27 19:51:33 +0000

    feat: tooltips adjust test

--- tests/components/tooltip/tooltip.js
+++ tests/components/tooltip/tooltip.js
@@ -11,7 +11,7 @@ export default {
       type: Object,
       default: [
         h("button", { class: "tooltip-toggle" }),
-        h("div", { class: "tooltip-description show" }, "Tips go here"),
+        h("span", { class: "tooltip-description show" }, "Tips go here"),
       ],
     },
     attributes: {
