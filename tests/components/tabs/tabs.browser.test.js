456ee2e83268bd7d65327c0c129646b0cdab8ad5
Jessica <jdash@coldfrontlabs.ca>
2026-07-27 14:07:59 +0000

    feat: tabs tests

--- tests/components/tabs/tabs.browser.test.js
+++ tests/components/tabs/tabs.browser.test.js
@@ -14,7 +14,7 @@ describe("Disclosure Component", () => {
 
       await document.fonts.ready;
 
-      const toggleButton = screen.getByRole("button").first();
+      const toggleButton = screen.getByRole("button", { selected: true });
       await toggleButton.click();
 
       await expect(toggleButton).toHaveAttribute("tabindex", "0");
