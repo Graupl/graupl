13f595d75434839f2f97154d01c3302759a3abf2
Jessica <jdash@coldfrontlabs.ca>
2026-07-27 19:41:10 +0000

    feat: accordion adjust test

--- tests/components/accordion/accordion.browser.test.js
+++ tests/components/accordion/accordion.browser.test.js
@@ -14,7 +14,7 @@ describe("Accordion Component", () => {
 
       await document.fonts.ready;
 
-      const toggleButton = screen.getByTestid("item-toggle").first();
+      const toggleButton = screen.getByTestid("item-toggle");
       await toggleButton.click();
 
       await expect(screen.getByTestId("accordion")).toMatchScreenshot();
