538a05d773305115c4c84b817177c5ffd8f582b9
Jessica <jdash@coldfrontlabs.ca>
2026-07-27 14:58:32 +0000

    feat: tooltips adjust test

--- tests/components/tooltip/tooltip.browser.test.js
+++ tests/components/tooltip/tooltip.browser.test.js
@@ -15,6 +15,7 @@ describe("Disclosure Component", () => {
       await document.fonts.ready;
 
       const toggleButton = screen.getByRole("button");
+      await toggleButton.click();
 
       await expect(toggleButton).toHaveClass("tooltip-toggle");
 
