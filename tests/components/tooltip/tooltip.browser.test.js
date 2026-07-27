06da9b75e6ce61556de8d4e41e00534782747707
Jessica <jdash@coldfrontlabs.ca>
2026-07-27 15:10:48 +0000

    feat: tooltips adjust test

--- tests/components/tooltip/tooltip.browser.test.js
+++ tests/components/tooltip/tooltip.browser.test.js
@@ -3,7 +3,7 @@ import { render } from "vitest-browser-vue";
 import Component from "./tooltip.js";
 import { variants } from "../defaults.js";
 
-describe("Disclosure Component", () => {
+describe("Tooltip Component", () => {
   describe.for(variants)("%s tooltip", async (variant) => {
     it("Should match screenshot", async () => {
       const screen = render(Component, {
