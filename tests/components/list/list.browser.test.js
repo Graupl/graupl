d0f55bdadde94e725ca17419d2d768fddcedf2d1
Jessica <jdash@coldfrontlabs.ca>
2026-07-28 17:50:42 +0000

    feat: menu and tooltip attempt test

--- tests/components/list/list.browser.test.js
+++ tests/components/list/list.browser.test.js
@@ -1,15 +1,14 @@
 import { describe, it, expect } from "vitest";
 import { render } from "vitest-browser-vue";
 import Component from "./list.js";
-import { variants, states } from "../defaults.js";
+import { variants } from "../defaults.js";
 
 describe("List Component", () => {
   describe.for(variants)("%s list", (variant) => {
-    it.each(states)("Should match screenshot with state: %s", async (state) => {
+    it("Should match screenshot", async () => {
       const screen = render(Component, {
         props: {
           variant,
-          state,
         },
       });
 
