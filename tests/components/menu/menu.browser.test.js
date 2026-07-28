0127383f3c7e6d7e92e8b85a3a80197eefb66a18
Jessica <jdash@coldfrontlabs.ca>
2026-07-28 20:00:05 +0000

    feat: menu test

--- tests/components/menu/menu.browser.test.js
+++ tests/components/menu/menu.browser.test.js
@@ -14,7 +14,7 @@ describe("Menu Component", () => {
 
       await document.fonts.ready;
 
-      const toggleButton = screen.getByRole("button");
+      const toggleButton = screen.getByRole("button").first();
       await toggleButton.click();
 
       await expect(toggleButton).toHaveAttribute("aria-expanded", "true");
