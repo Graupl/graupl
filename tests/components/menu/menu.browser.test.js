5fa7f19baba3cb805a298d9d4e4772931345d2cf
Jessica <jdash@coldfrontlabs.ca>
2026-07-29 18:58:27 +0000

    feat: menu test

--- tests/components/menu/menu.browser.test.js
+++ tests/components/menu/menu.browser.test.js
@@ -1,8 +1,7 @@
-import { describe, it, expect } from "vitest";
-import { render } from "vitest-browser-vue";
+import { describe, it, expect, afterEach } from "vitest";
+import { cleanup, render } from "vitest-browser-vue";
 import Component from "./menu.js";
 import { variants } from "../defaults.js";
-import { userEvent } from "vitest/browser";
 
 describe("Menu Component", () => {
   describe.for(variants)("%s menu", (variant) => {
@@ -13,16 +12,18 @@ describe("Menu Component", () => {
         },
       });
 
-      const user = userEvent.setup();
-
       await document.fonts.ready;
 
       const menuButton = screen.getByRole("button", { name: "Button" }).first();
-      await user.click(menuButton);
+      await menuButton.click();
 
-      await expect(menuButton).toHaveBeenCalledOnce;
+      await expect(menuButton).toHaveAttribute("aria-expanded", "true");
 
       await expect(screen.getByTestId("menu")).toMatchScreenshot();
     });
   });
+});
+
+afterEach(() => {
+  cleanup();
 });
