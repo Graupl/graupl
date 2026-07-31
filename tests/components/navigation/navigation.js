b908e39244e2ed1ee575d47740e8f2da0f60a92e
Jessica <jdash@coldfrontlabs.ca>
2026-07-31 19:43:51 +0000

    feat: navigation test

---
+++ tests/components/navigation/navigation.js
@@ -0,0 +1,79 @@
+import { h } from "vue";
+import AdvancedComponent from "../component/advanced-component.js";
+import BasicComponent from "../component/basic-component.js";
+import { setupClasses } from "../helpers.js";
+
+export default {
+  props: {
+    attributes: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const attributes = setupClasses(props.attributes, ["navigation"]);
+
+    return () =>
+      h(AdvancedComponent, {
+        ...props,
+        attributes,
+        children: [
+          h(BasicComponent, {
+            tag: "a",
+            attributes: {
+              class: ["navigation-branding"],
+              href: "#",
+            },
+            text: "Branding",
+          }),
+          h(BasicComponent, {
+            tag: "button",
+            attributes: {
+              class: ["navigation-toggle", "ml-auto", "mr-0"],
+            },
+          }),
+          h(AdvancedComponent, {
+            tag: "ul",
+            attributes: { class: ["menu"] },
+            children: [
+              h(AdvancedComponent, {
+                tag: "li",
+                attributes: { class: ["menu-item", "submenu-item"] },
+                children: [
+                  h(BasicComponent, {
+                    tag: "a",
+                    attributes: {
+                      class: ["menu-link"],
+                    },
+                    text: "Button",
+                  }),
+                ],
+              }),
+              h(AdvancedComponent, {
+                tag: "li",
+                attributes: { class: ["menu-item"] },
+                children: [
+                  h(BasicComponent, {
+                    tag: "a",
+                    attributes: { class: ["menu-link"], href: "#" },
+                    text: "List Content ",
+                  }),
+                ],
+              }),
+              h(AdvancedComponent, {
+                tag: "li",
+                attributes: { class: ["menu-item"] },
+                children: [
+                  h(BasicComponent, {
+                    tag: "a",
+                    attributes: { class: ["menu-link"], href: "#" },
+                    text: "List Content 2",
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
+      });
+  },
+};
