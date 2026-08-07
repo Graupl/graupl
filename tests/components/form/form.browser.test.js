82f7a8f12d1abbf0b3c9ffa357d11759c9d6a6f0
Jessica <jdash@coldfrontlabs.ca>
2026-08-07 19:56:49 +0000

    feat: base form test

--- tests/components/form/form.browser.test.js
+++ tests/components/form/form.browser.test.js
@@ -1,22 +1,37 @@
 import { describe, it, expect } from "vitest";
 import { render } from "vitest-browser-vue";
 import Component from "./form.js";
-import { variants, states } from "../defaults.js";
+import { states } from "../defaults.js";
 
 describe("Form Component", () => {
-  describe.for(variants)("%s form", (variant) => {
-    it.each(states)("Should match screenshot with state: %s", async (state) => {
-      const screen = render(Component, {
-        props: {
-          variant,
-          state,
+  it.each(states)("Should match screenshot with state: %s", async (state) => {
+    const screen = render(Component, {
+      props: {
+        state,
+        attributes: {
           "data-testid": "form",
+          value: true,
         },
-      });
-
-      await document.fonts.ready;
-
-      await expect(screen.getByTestId("form")).toMatchScreenshot();
+      },
     });
+
+    await document.fonts.ready;
+
+    await expect(screen.getByTestId("form")).toMatchScreenshot();
+  });
+
+  it("Should match screenshot with changed attributes:", async () => {
+    const screen = render(Component, {
+      props: {
+        attributes: {
+          "data-testid": "form",
+          value: true,
+        },
+      },
+    });
+
+    await document.fonts.ready;
+
+    await expect(screen.getByTestId("form")).toMatchScreenshot();
   });
 });
--- tests/components/form/form.js
+++ tests/components/form/form.js
@@ -5,198 +5,13 @@ import { setupClasses } from "../helpers.js";
 
 export default {
   props: {
-    variant: {
+    state: {
       type: String,
       default: "",
     },
-    children: {
-      type: Array,
-      default: () => [
-        h(AdvancedComponent, {
-          attributes: { class: ["input-group"] },
-          children: [
-            h(BasicComponent, {
-              tag: "input",
-              attributes: {
-                type: "input",
-                placeholder: "Enter text",
-                id: "input",
-              },
-            }),
-            h(BasicComponent, {
-              tag: "label",
-              attributes: { for: "input" },
-              text: "Input",
-            }),
-            h(BasicComponent, {
-              tag: "small",
-              attributes: { class: ["help-text"] },
-              text: "This is the help text for this input.",
-            }),
-          ],
-        }),
-        h(BasicComponent, {
-          tag: "textarea",
-          attributes: {
-            type: "textarea",
-            placeholder: "Enter text",
-            id: "textarea",
-          },
-        }),
-        h(BasicComponent, {
-          tag: "label",
-          attributes: { for: "textarea" },
-          text: "Textarea",
-        }),
-        h(AdvancedComponent, {
-          attributes: { class: ["input-group", "inline"] },
-          children: [
-            h(AdvancedComponent, {
-              tag: "select",
-              attributes: {
-                type: "select ",
-                id: "select",
-              },
-              children: [
-                h(BasicComponent, {
-                  tag: "option",
-                  attributes: { value: "Value Default" },
-                  text: "Select an option",
-                }),
-                h(AdvancedComponent, {
-                  tag: "optgroup",
-                  attributes: { label: "group1" },
-                  children: [
-                    h(BasicComponent, {
-                      tag: "option",
-                      attributes: { value: "group1option1" },
-                      text: "Option 1",
-                    }),
-                    h(BasicComponent, {
-                      tag: "option",
-                      attributes: { value: "group1option2" },
-                      text: "Option 2",
-                    }),
-                  ],
-                }),
-              ],
-            }),
-            h(BasicComponent, {
-              tag: "label",
-              attributes: { for: "select" },
-              text: "Select",
-            }),
-          ],
-        }),
-        h(AdvancedComponent, {
-          attributes: { class: ["input-group"] },
-          children: [
-            h(AdvancedComponent, {
-              tag: "fieldset",
-              children: [
-                h(BasicComponent, {
-                  tag: "legend",
-                  text: "Checkboxes",
-                }),
-                h(AdvancedComponent, {
-                  attributes: { class: ["input-group", "inline"] },
-                  children: [
-                    h(BasicComponent, {
-                      tag: "input",
-                      attributes: { type: "checkbox" },
-                      id: "checkbox1",
-                    }),
-                    h(BasicComponent, {
-                      tag: "label",
-                      attributes: { for: "checkbox1" },
-                      text: "Checkbox 1",
-                    }),
-                  ],
-                }),
-                h(AdvancedComponent, {
-                  attributes: { class: ["input-group", "inline"] },
-                  children: [
-                    h(BasicComponent, {
-                      tag: "input",
-                      attributes: { type: "checkbox" },
-                      id: "Checkbox2",
-                    }),
-                    h(BasicComponent, {
-                      tag: "label",
-                      attributes: { for: "Checkbox2" },
-                      text: "Checkbox 2",
-                    }),
-                  ],
-                }),
-              ],
-            }),
-          ],
-        }),
-        h(AdvancedComponent, {
-          attributes: { class: ["input-group"] },
-          children: [
-            h(AdvancedComponent, {
-              tag: "fieldset",
-              children: [
-                h(BasicComponent, {
-                  tag: "legend",
-                  text: "Radio Buttons",
-                }),
-                h(AdvancedComponent, {
-                  attributes: { class: ["input-group", "inline"] },
-                  children: [
-                    h(BasicComponent, {
-                      tag: "input",
-                      attributes: { type: "radio", name: "radio1" },
-                      id: "radio1",
-                    }),
-                    h(BasicComponent, {
-                      tag: "label",
-                      attributes: { for: "radio1" },
-                      text: "Radio 1",
-                    }),
-                  ],
-                }),
-                h(AdvancedComponent, {
-                  attributes: { class: ["input-group", "inline"] },
-                  children: [
-                    h(BasicComponent, {
-                      tag: "input",
-                      attributes: { type: "radio", name: "radio2" },
-                      id: "radio2",
-                    }),
-                    h(BasicComponent, {
-                      tag: "label",
-                      attributes: { for: "radio2" },
-                      text: "Radio 2",
-                    }),
-                  ],
-                }),
-              ],
-            }),
-          ],
-        }),
-        h(AdvancedComponent, {
-          attributes: { class: ["input-group"] },
-          children: [
-            h(BasicComponent, {
-              tag: "label",
-              attributes: { for: "range" },
-              text: "Range",
-            }),
-            h(BasicComponent, {
-              tag: "input",
-              attributes: {
-                type: "range",
-                id: "range",
-                min: 0,
-                max: 100,
-                step: 1,
-              },
-            }),
-          ],
-        }),
-      ],
+    value: {
+      type: Boolean,
+      default: false,
     },
     attributes: {
       type: Object,
@@ -206,6 +21,265 @@ export default {
   setup(props) {
     const attributes = setupClasses(props.attributes, ["container"]);
 
-    return () => h(AdvancedComponent, { ...props, attributes, tag: "form" });
+    return () =>
+      h(AdvancedComponent, {
+        ...props,
+        attributes,
+        tag: "form",
+        children: [
+          h(AdvancedComponent, {
+            attributes: { class: ["input-group"] },
+            children: [
+              h(BasicComponent, {
+                tag: "input",
+                attributes: {
+                  type: "input",
+                  placeholder: "Enter text",
+                  id: "input",
+                  value: props.value ? "" : "Value",
+                },
+              }),
+              h(BasicComponent, {
+                tag: "label",
+                attributes: { for: "input" },
+                text: "Input",
+              }),
+              h(BasicComponent, {
+                tag: "small",
+                attributes: { class: ["help-text"] },
+                text: "This is the help text for this input.",
+              }),
+            ],
+          }),
+          h(BasicComponent, {
+            tag: "input",
+            attributes: {
+              type: "input",
+              placeholder: "Input Disabled",
+              id: "input-disabled",
+              disabled: props.value ? false : true,
+            },
+          }),
+          h(BasicComponent, {
+            tag: "label",
+            attributes: { for: "input-disabled" },
+            text: "Input Disabled",
+          }),
+
+          h(BasicComponent, {
+            tag: "textarea",
+            attributes: {
+              type: "textarea-disabled",
+              placeholder: "Enter text",
+              id: "textarea-disabled",
+              disabled: props.value ? false : true,
+            },
+          }),
+          h(BasicComponent, {
+            tag: "label",
+            attributes: { for: "textarea-disabled" },
+            text: "Textarea Disabled",
+          }),
+          h(AdvancedComponent, {
+            attributes: {
+              class: ["input-group", "inline"],
+              disabled: props.value ? false : true,
+            },
+            children: [
+              h(AdvancedComponent, {
+                tag: "select",
+                attributes: {
+                  type: "select",
+                  id: "select-disabled",
+                  disabled: props.value ? false : true,
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "option",
+                    attributes: { value: "Value-disabled" },
+                    text: "Disabled option",
+                  }),
+                ],
+              }),
+              h(BasicComponent, {
+                tag: "label",
+                attributes: { for: "select-disabled" },
+                text: "Select-disabled",
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            attributes: { class: ["input-group", "inline"] },
+            children: [
+              h(AdvancedComponent, {
+                tag: "select",
+                attributes: {
+                  type: "select ",
+                  id: "select",
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "option",
+                    attributes: { value: "Value Default" },
+                    text: "Select an option",
+                  }),
+                  h(AdvancedComponent, {
+                    tag: "optgroup",
+                    attributes: { label: "group1" },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "option",
+                        attributes: { value: "group1option1" },
+                        text: "Option 1",
+                      }),
+                      h(BasicComponent, {
+                        tag: "option",
+                        attributes: { value: "group1option2" },
+                        text: "Option 2",
+                        selected: props.value ? false : true,
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              h(BasicComponent, {
+                tag: "label",
+                attributes: { for: "select-disable" },
+                text: "Select",
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            tag: "fieldset",
+            checked: props.value ? false : true,
+            children: [
+              h(BasicComponent, {
+                tag: "legend",
+                text: "Checkboxes",
+              }),
+              h(AdvancedComponent, {
+                attributes: { class: ["input-group", "inline"] },
+                children: [
+                  h(BasicComponent, {
+                    tag: "input",
+                    attributes: { type: "checkbox" },
+                    id: "checkbox1",
+                  }),
+                ],
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            attributes: { class: ["input-group"] },
+            children: [
+              h(AdvancedComponent, {
+                tag: "fieldset",
+                children: [
+                  h(BasicComponent, {
+                    tag: "legend",
+                    text: "Checkboxes",
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: { class: ["input-group", "inline"] },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "input",
+                        attributes: { type: "checkbox" },
+                        id: "checkbox1",
+                      }),
+                      h(BasicComponent, {
+                        tag: "label",
+                        attributes: { for: "checkbox1" },
+                        text: "Checkbox 1",
+                        checked: props.value ? false : true,
+                      }),
+                    ],
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: { class: ["input-group", "inline"] },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "input",
+                        attributes: { type: "checkbox" },
+                        id: "Checkbox2",
+                      }),
+                      h(BasicComponent, {
+                        tag: "label",
+                        attributes: { for: "Checkbox2" },
+                        text: "Checkbox 2",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            attributes: { class: ["input-group"] },
+            children: [
+              h(AdvancedComponent, {
+                tag: "fieldset",
+                children: [
+                  h(BasicComponent, {
+                    tag: "legend",
+                    text: "Radio Buttons",
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: { class: ["input-group", "inline"] },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "input",
+                        attributes: { type: "radio", name: "radio1" },
+                        id: "radio1",
+                      }),
+                      h(BasicComponent, {
+                        tag: "label",
+                        attributes: { for: "radio1" },
+                        text: "Radio 1",
+                      }),
+                    ],
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: { class: ["input-group", "inline"] },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "input",
+                        attributes: { type: "radio", name: "radio2" },
+                        id: "radio2",
+                        checked: props.value ? false : true,
+                      }),
+                      h(BasicComponent, {
+                        tag: "label",
+                        attributes: { for: "radio2" },
+                        text: "Radio 2",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            attributes: { class: ["input-group"] },
+            children: [
+              h(BasicComponent, {
+                tag: "label",
+                attributes: { for: "range" },
+                text: "Range",
+              }),
+              h(BasicComponent, {
+                tag: "input",
+                attributes: {
+                  type: "range",
+                  id: "range",
+                  min: 0,
+                  max: 100,
+                  step: 1,
+                },
+              }),
+            ],
+          }),
+        ],
+      });
   },
 };
