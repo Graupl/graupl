adbe6bb21b90c4b21f0f1e764b7ef45c7b72c29d
Jessica <jdash@coldfrontlabs.ca>
2026-08-10 19:56:54 +0000

    feat: setup input groups

---
+++ tests/components/input-groups/input-groups.browser.test.js
@@ -0,0 +1,46 @@
+import { describe, it, expect } from "vitest";
+import { render } from "vitest-browser-vue";
+import Component from "./input-groups.js";
+import { userEvent } from "vitest/browser";
+
+describe("Form Component", () => {
+  it("Should match screenshot when normal:", async () => {
+    const screen = render(Component, {
+      props: {
+        attributes: {
+          "data-testid": "input-groups",
+        },
+      },
+    });
+
+    await document.fonts.ready;
+
+    await expect(screen.getByTestId("input-groups")).toMatchScreenshot();
+  });
+
+  it("Should match screenshot with changed attributes:", async () => {
+    const screen = render(Component, {
+      props: {
+        attributes: {
+          "data-testid": "input-groups",
+          value: true,
+        },
+      },
+    });
+
+    await document.fonts.ready;
+    const user = userEvent.setup();
+    const selecter = screen.getByTestId("selecter1");
+    const selecter2 = screen.getByTestId("selecter2");
+    const checkedOption = screen.getByTestId("checkedoption1");
+    const checkedOption2 = screen.getByTestId("checkedoption2");
+
+    // Set the element value directly to the option's value attribute
+    await user.selectOptions(selecter, "Option 2");
+    await user.selectOptions(selecter2, ["Default Value", "Option 2"]);
+    await user.click(checkedOption);
+    await user.click(checkedOption2);
+
+    await expect(screen.getByTestId("input-groups")).toMatchScreenshot();
+  });
+});
