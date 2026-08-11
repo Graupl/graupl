fae66567cc58ed9a76f9aa8cc337ead40727b942
Jessica <jdash@coldfrontlabs.ca>
2026-08-11 15:35:05 +0000

    feat: table test

---
+++ tests/components/table/table.browser.test.js
@@ -0,0 +1,32 @@
+import { describe, it, expect } from "vitest";
+import { render } from "vitest-browser-vue";
+import Component from "./table.js";
+import { variants } from "../defaults.js";
+
+const classes = [
+  "responsive-table",
+  "bordered",
+  "hoverable",
+  "striped-columns",
+  "striped-rows",
+];
+
+describe("Table Component", () => {
+  describe.for(variants)("%s table", async (variant) => {
+    it.each(classes)("Should match screenshot:", async (classes) => {
+      const screen = render(Component, {
+        props: {
+          variant,
+          classes,
+          attributes: {
+            "data-testid": "table",
+          },
+        },
+      });
+
+      await document.fonts.ready;
+
+      await expect(screen.getByTestId("table")).toMatchScreenshot();
+    });
+  });
+});
