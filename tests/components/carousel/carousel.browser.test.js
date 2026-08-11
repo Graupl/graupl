4db41ea987cef418a6fa28de7e3c02dbb9cd4680
Jessica <jdash@coldfrontlabs.ca>
2026-08-11 17:21:14 +0000

    feat: carousel test

---
+++ tests/components/carousel/carousel.browser.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { render } from "vitest-browser-vue";
+import Component from "./carousel.js";
+
+describe("Carousel Component", () => {
+  it("Should match screenshot:", async () => {
+    const screen = render(Component, {
+      props: {
+        attributes: {
+          "data-testid": "carousel",
+        },
+      },
+    });
+
+    await document.fonts.ready;
+
+    await expect(screen.getByTestId("carousel")).toMatchScreenshot();
+  });
+});
---
+++ tests/components/carousel/carousel.js
@@ -0,0 +1,269 @@
+import { h } from "vue";
+import AdvancedComponent from "../component/advanced-component.js";
+import BasicComponent from "../component/basic-component.js";
+import { setupClasses } from "../helpers.js";
+
+export default {
+  props: {
+    attributes: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const attributes = setupClasses(props.attributes, ["carousel"]);
+
+    return () =>
+      h(AdvancedComponent, {
+        tag: "section",
+        ...props,
+        attributes,
+        children: [
+          h(AdvancedComponent, {
+            attributes: {
+              class: ["carousel-control-container"],
+            },
+            children: [
+              h(BasicComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-control", "autoplay", "pause"],
+                  "aria-label": "Pause",
+                },
+              }),
+              h(BasicComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-control", "previous"],
+                  "aria-label": "Previous slide",
+                },
+              }),
+              h(BasicComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-control", "next"],
+                  "aria-label": "Next slide",
+                },
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            attributes: {
+              class: ["carousel-tab-container"],
+              role: "tablist",
+            },
+            children: [
+              h(AdvancedComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-tab"],
+                  "aria-label": "Carousel item 1",
+                  role: "tab",
+                },
+              }),
+              h(AdvancedComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-tab"],
+                  "aria-label": "Carousel item 2",
+                  role: "tab",
+                },
+              }),
+              h(AdvancedComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-tab"],
+                  "aria-label": "Carousel item 3",
+                  role: "tab",
+                },
+              }),
+              h(AdvancedComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-tab"],
+                  "aria-label": "Carousel item 4",
+                  role: "tab",
+                },
+              }),
+              h(AdvancedComponent, {
+                tag: "button",
+                attributes: {
+                  class: ["carousel-tab"],
+                  "aria-label": "Carousel item 5",
+                  role: "tab",
+                },
+              }),
+            ],
+          }),
+          h(AdvancedComponent, {
+            attributes: {
+              class: ["carousel-item-container"],
+              tabindex: "-1",
+            },
+            children: [
+              h(AdvancedComponent, {
+                attributes: {
+                  class: ["carousel-item", "active"],
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "img",
+                    attributes: {
+                      src: "slide1.jpg",
+                      alt: "Slide 1 placeholder alt text",
+                    },
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: {
+                      class: [
+                        "position-absolute",
+                        "bottom-0",
+                        "left-0",
+                        "right-0",
+                        `bg-tertiary-100`,
+                        "p-5",
+                      ],
+                    },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "p",
+                        text: "This is a slide! You can put any content you want here.",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              h(AdvancedComponent, {
+                attributes: {
+                  class: ["carousel-item"],
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "img",
+                    attributes: {
+                      src: "slide2.jpg",
+                      alt: "Slide 2 placeholder alt text",
+                    },
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: {
+                      class: [
+                        "position-absolute",
+                        "bottom-0",
+                        "left-0",
+                        "right-0",
+                        `bg-tertiary-100`,
+                        "p-5",
+                      ],
+                    },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "p",
+                        text: "This is another slide! You can put any content you want here.",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              h(AdvancedComponent, {
+                attributes: {
+                  class: ["carousel-item"],
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "img",
+                    attributes: {
+                      src: "slide3.jpg",
+                      alt: "Slide 3 placeholder alt text",
+                    },
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: {
+                      class: [
+                        "position-absolute",
+                        "bottom-0",
+                        "left-0",
+                        "right-0",
+                        `bg-tertiary-100`,
+                        "p-5",
+                      ],
+                    },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "p",
+                        text: "This is yet another slide! You can put any content you want here.",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              h(AdvancedComponent, {
+                attributes: {
+                  class: ["carousel-item"],
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "img",
+                    attributes: {
+                      src: "slide4.jpg",
+                      alt: "Slide 4 placeholder alt text",
+                    },
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: {
+                      class: [
+                        "position-absolute",
+                        "bottom-0",
+                        "left-0",
+                        "right-0",
+                        `bg-tertiary-100`,
+                        "p-5",
+                      ],
+                    },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "p",
+                        text: "This is yet another slide! You can put any content you want here.",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              h(AdvancedComponent, {
+                attributes: {
+                  class: ["carousel-item"],
+                },
+                children: [
+                  h(BasicComponent, {
+                    tag: "img",
+                    attributes: {
+                      src: "slide5.jpg",
+                      alt: "Slide 5 placeholder alt text",
+                    },
+                  }),
+                  h(AdvancedComponent, {
+                    attributes: {
+                      class: [
+                        "position-absolute",
+                        "bottom-0",
+                        "left-0",
+                        "right-0",
+                        `bg-tertiary-100`,
+                        "p-5",
+                      ],
+                    },
+                    children: [
+                      h(BasicComponent, {
+                        tag: "p",
+                        text: "This is yet another slide! You can put any content you want here.",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
+      });
+  },
+};
