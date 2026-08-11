b091ec9813e3df3a3513c93eb9a4ec48d4cc6732
Jessica <jdash@coldfrontlabs.ca>
2026-08-11 19:22:50 +0000

    feat: columns test

---
+++ tests/components/columns/columns.js
@@ -0,0 +1,52 @@
+import { h } from "vue";
+import AdvancedComponent from "../component/advanced-component.js";
+import BasicComponent from "../component/basic-component.js";
+import { setupClasses } from "../helpers.js";
+
+export default {
+  props: {
+    children: {
+      type: Array,
+      default: [
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+        h(BasicComponent, {
+          attributes: { class: ["bg-primary", "py-7", "px-5"] },
+        }),
+      ],
+    },
+    attributes: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const attributes = setupClasses(props.attributes, [
+      "columns",
+      "count-4",
+      "py-5",
+      "g-5",
+    ]);
+
+    return () => h(AdvancedComponent, { ...props, attributes });
+  },
+};
