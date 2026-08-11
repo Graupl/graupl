4c0314f5786eb75bfb29a4a908f5b62777b6d6b9
Jessica <jdash@coldfrontlabs.ca>
2026-08-11 21:53:06 +0000

    feat: carousel test

--- tests/components/carousel/carousel.browser.test.js
+++ tests/components/carousel/carousel.browser.test.js
@@ -1,9 +1,10 @@
 import { describe, it, expect } from "vitest";
 import { render } from "vitest-browser-vue";
 import Component from "./carousel.js";
+import { userEvent } from "vitest/browser";
 
 describe("Carousel Component", () => {
-  it("Should match screenshot", async () => {
+  it("Should match screenshot with pause", async () => {
     const screen = render(Component, {
       props: {
         attributes: {
@@ -13,6 +14,27 @@ describe("Carousel Component", () => {
     });
 
     await document.fonts.ready;
+
+    await expect(screen.getByTestId("carousel")).toMatchScreenshot();
+  });
+});
+
+describe("Carousel Component", () => {
+  it("Should match screenshot with play", async () => {
+    const screen = render(Component, {
+      props: {
+        attributes: {
+          "data-testid": "carousel",
+        },
+      },
+    });
+
+    await document.fonts.ready;
+
+    const user = userEvent.setup();
+    const pauseButton = screen.getByRole("button", { class: "autoplay" });
+
+    await user.click(pauseButton);
 
     await expect(screen.getByTestId("carousel")).toMatchScreenshot();
   });
