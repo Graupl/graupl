3fb1d507c9aee4e9cdfed2a50385f5e61469a7db
Jessica <jdash@coldfrontlabs.ca>
2026-08-11 16:25:14 +0000

    feat: table test

--- tests/components/table/table.browser.test.js
+++ tests/components/table/table.browser.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest";
 import { render } from "vitest-browser-vue";
 import Component from "./table.js";
 import { variants } from "../defaults.js";
+import { userEvent } from "vitest/browser";
 
 const classes = [
   "responsive-table",
@@ -24,6 +25,11 @@ describe("Table Component", () => {
       });
 
       await document.fonts.ready;
+
+      const user = userEvent.setup();
+      const cell = screen.getByText("JavaScript frameworks");
+
+      await user.hover(cell);
 
       await expect(screen.getByTestId("table")).toMatchScreenshot();
     });
