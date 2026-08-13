5aecf0f331a25bb98ede66a8235f4af02af1f641
Nick Milton <git@ndjm.ca>
2026-08-13 01:56:28 +0000

    test(vrt): increase size of smaller tests for less random failures

--- tests/components/tooltip/tooltip.browser.test.js
+++ tests/components/tooltip/tooltip.browser.test.js
@@ -4,31 +4,22 @@ import Component from "./tooltip.js";
 
 describe("Tooltip Component", () => {
   it("Should match screenshot when closed", async () => {
-    const screen = render(Component, {
-      props: {
-        attributes: {
-          "data-testid": "tooltip",
-        },
-      },
-    });
+    const screen = render(Component);
 
     await document.fonts.ready;
 
-    await expect(screen.getByTestId("tooltip")).toMatchScreenshot();
+    await expect(screen.getByTestId("tooltip-wrapper")).toMatchScreenshot();
   });
 
   it("Should match screenshot when open", async () => {
     const screen = render(Component, {
       props: {
         open: true,
-        attributes: {
-          "data-testid": "tooltip",
-        },
       },
     });
 
     await document.fonts.ready;
 
-    await expect(screen.getByTestId("tooltip")).toMatchScreenshot();
+    await expect(screen.getByTestId("tooltip-wrapper")).toMatchScreenshot();
   });
 });
