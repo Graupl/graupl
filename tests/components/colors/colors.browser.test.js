4f3c9199fbacf3bc30c6ddd6d860c28ee45edbdd
Jessica <jdash@coldfrontlabs.ca>
2026-08-18 20:53:23 +0000

    test(vrt): colors test

--- tests/components/colors/colors.browser.test.js
+++ tests/components/colors/colors.browser.test.js
@@ -8,14 +8,14 @@ const shades = ["100", "200", "300", "400", "500", "600", "700", "800", "900"];
 describe("Colors Component", () => {
   describe.for(variants)("%s color", async (variant) => {
     let variantShade = "";
-    if (variants != "default") {
+    if (variant !== "default") {
       shades.forEach((shade) => {
         variantShade = `bg-${variant}-${shade}`;
         it(`Should match ${variant}-${shade}`, async () => {
           const screen = render(Component, {
             props: {
+              variantShade,
               attributes: {
-                class: [variantShade],
                 "data-testid": "color",
               },
             },
